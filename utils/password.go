5ff4ea7d7bcb98dcbde0c6bede3f48ea465ce0a4
Meshach Odejide <meshacholuwadamilare@gmail.com>
2023-02-27 04:27:04 +0100

    add password for password helper functions

---
+++ utils/password.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("could not hash password", err)
+	}
+	return string(hashedPassword), nil
+}
+
+func VerifyPassword(hashedPassword, candidatePassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(candidatePassword))
+
+}
