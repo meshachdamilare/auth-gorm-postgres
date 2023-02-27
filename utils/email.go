c19248f1bbd9063d8331a73b785dc8fc216d3768
Meshach Odejide <meshacholuwadamilare@gmail.com>
2023-02-27 17:05:40 +0100

    add function to send email

---
+++ utils/email.go
@@ -0,0 +1,79 @@
+package utils
+
+import (
+	"bytes"
+	"crypto/tls"
+	"github.com/k3a/html2text"
+	"github.com/meshachdamilare/auth-with-gorm-postgres/config"
+	"github.com/meshachdamilare/auth-with-gorm-postgres/models"
+	"gopkg.in/gomail.v2"
+	"html/template"
+	"io/fs"
+	"log"
+	"path/filepath"
+)
+
+type EmailData struct {
+	URL       string
+	FirstName string
+	Subject   string
+}
+
+func ParseTemplateDir(dir string) (*template.Template, error) {
+	var paths []string
+	err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			paths = append(paths, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return template.ParseFiles(paths...)
+}
+
+func SendEmail(user *models.User, data *EmailData) {
+	conf, err := config.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("could not load config", err)
+	}
+
+	// Sender data.
+	from := conf.EmailFrom
+	smtpPass := conf.SMTPPass
+	smtpUser := conf.SMTPUser
+	to := user.Email
+	smtpHost := conf.SMTPHost
+	smtpPort := conf.SMTPPort
+
+	var body bytes.Buffer
+
+	template, err := ParseTemplateDir("templates")
+	if err != nil {
+		log.Fatal("Could not parse template", err)
+	}
+
+	template.ExecuteTemplate(&body, "verificationCode.html", &data)
+
+	m := gomail.NewMessage()
+
+	m.SetHeader("From", from)
+	m.SetHeader("To", to)
+	m.SetHeader("Subject", data.Subject)
+	m.SetBody("text/html", body.String())
+	m.AddAlternative("text/plain", html2text.HTML2Text(body.String()))
+
+	d := gomail.NewDialer(smtpHost, smtpPort, smtpUser, smtpPass)
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	// Send Email
+	if err := d.DialAndSend(m); err != nil {
+		log.Fatal("Could not send email: ", err)
+	}
+
+}
